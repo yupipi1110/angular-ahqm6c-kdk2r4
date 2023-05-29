f54c08e4234c89154457ebad2e25ba617a264d65
yupipi1110 <126690147+yupipi1110@users.noreply.github.com>
2023-05-29 11:46:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ahqm6c-kdk2r4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ahqm6c-kdk2r4)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+
+
+export class ProductDetailsComponent {
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
+
+}
+
